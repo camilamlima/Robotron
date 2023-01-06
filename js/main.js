b4d8337ba1a6c24a7771878e9283f642e6c434f3
Camila Lima <camila.bruschetta@ab-inbev.com>
2023-01-05 23:26:42 -0300

    Projeto Robotron trabalhando com Array

--- js/main.js
+++ js/main.js
@@ -1,8 +1,35 @@
-const robotron = document.querySelector("#robotron");
+const subtrair = document.querySelector("#subtrair");
+const somar = document.querySelector("#somar");
+const braco = document.querySelector("#braco");
 
-robotron.addEventListener("click", dizOi);
+const controle = document.querySelectorAll(".controle-ajuste");
 
-function dizOi() {
-  console.log("Oiiiii");
-  console.log("Bem vindo");
+controle.forEach((elemento) => {
+  elemento.addEventListener("click", (evento) => {
+    console.log(evento.target.textContent);
+  });
+});
+
+function manipulaDados(operacao) {
+  if (operacao === "subtrair") {
+    braco.value = parseInt(braco.value) - 1;
+  } else {
+    braco.value = parseInt(braco.value) + 1;
+  }
 }
+
+// const robotron = document.querySelector("#robotron");
+
+// //Função anônima só vai ser executada naquele momento, ela não pode ser chamada posteriormente
+// robotron.addEventListener("click", (evento) => {
+//   console.log(evento);
+// });
+
+// robotron.addEventListener("click", dizOi);
+
+// function dizOi(nome) {
+//   console.log("Oi " + nome);
+//   console.log("Bem vindo");
+// }
+
+// dizOi("Camila");
